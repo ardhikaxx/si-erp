d70189a4e1a4709fc9de138ef97fae76b8e1d2fc
ardhikaxx <ardhikayanuar58@gmail.com>
2026-07-21 15:43:29 +0700

    feat: penambahan model PaymentMethod

---
+++ app/Models/PaymentMethod.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class PaymentMethod extends Model
+{
+    use SoftDeletes;
+
+    protected $table = 'payment_methods';
+
+    protected $fillable = ['name', 'code', 'description', 'is_active'];
+
+    protected function casts(): array
+    {
+        return ['is_active' => 'boolean'];
+    }
+}
