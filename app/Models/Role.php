68032fde4c65b3f0c17cf9eed46c6dc4de64b6e9
ardhikaxx <ardhikayanuar58@gmail.com>
2026-07-21 15:41:26 +0700

    feat: penambahan model Role

---
+++ app/Models/Role.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Role extends Model
+{
+    protected $fillable = [
+        'name',
+        'guard_name',
+        'is_system',
+        'description',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'is_system' => 'boolean',
+        ];
+    }
+
+    public function permissions(): BelongsToMany
+    {
+        return $this->belongsToMany(Permission::class, 'role_permission');
+    }
+
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class, 'user_has_roles');
+    }
+}
