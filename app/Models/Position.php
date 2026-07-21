5b81e90e022bdaa213c25bcc773d966c7ad1773c
ardhikaxx <ardhikayanuar58@gmail.com>
2026-07-21 15:41:10 +0700

    feat: penambahan model Position

---
+++ app/Models/Position.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Position extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = ['name', 'code', 'department_id', 'description', 'is_active'];
+
+    protected function casts(): array
+    {
+        return ['is_active' => 'boolean'];
+    }
+
+    public function department()
+    {
+        return $this->belongsTo(Department::class);
+    }
+
+    public function employees()
+    {
+        return $this->hasMany(Employee::class);
+    }
+}
