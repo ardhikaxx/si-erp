8021e093efa770d68f267eb1569d95d60d3619c2
ardhikaxx <ardhikayanuar58@gmail.com>
2026-07-21 15:50:37 +0700

    feat: penambahan view index departemen

---
+++ resources/views/master_data/departments/index.blade.php
@@ -0,0 +1,51 @@
+﻿@extends('layouts.app')
+@section('title', 'Departemen')
+@section('content')
+@include('components.breadcrumb', ['breadcrumbs' => [
+    ['label' => 'Master Data', 'url' => '#'],
+    ['label' => 'Departemen'],
+]])
+<div class="card">
+    <div class="card-header d-flex justify-content-between align-items-center">
+        <h5 class="mb-0"><i class="fas fa-sitemap me-2"></i>Data Departemen</h5>
+        <a href="{{ route('master-data.departments.create') }}" class="btn btn-primary btn-sm"><i class="fas fa-plus"></i> Tambah Data</a>
+    </div>
+    <div class="card-body">
+        <table id="dataTable" class="table table-bordered table-striped w-100">
+            <thead>
+                <tr>
+                    <th>No</th>
+                    <th>Kode</th>
+                    <th>Nama Departemen</th>
+                    <th>Deskripsi</th>
+                    <th>Status</th>
+                    <th>Aksi</th>
+                </tr>
+            </thead>
+        </table>
+    </div>
+</div>
+@endsection
+@push('scripts')
+<script>
+$(document).ready(function() {
+    $('#dataTable').DataTable({
+        processing: true,
+        serverSide: true,
+        ajax: '{{ route("master-data.departments.index") }}',
+        columns: [
+            { data: 'DT_RowIndex', name: 'DT_RowIndex', orderable: false, searchable: false },
+            { data: 'code', name: 'code' },
+            { data: 'name', name: 'name' },
+            { data: 'description', name: 'description' },
+            { data: 'status', name: 'status' },
+            { data: 'action', name: 'action', orderable: false, searchable: false }
+        ],
+        responsive: true,
+        order: [[1, 'asc']],
+        language: { url: '//cdn.datatables.net/plug-ins/1.13.7/i18n/id.json' }
+    });
+});
+</script>
+@endpush
+
